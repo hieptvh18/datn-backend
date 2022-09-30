5b8585961582dd908c7906d87df7fa5ae186056a
hieptvh <hieptvh18@gmail.com>
2022-09-30 23:54:06 +0700

    PKNKD-58 Create reate module type product

--- app/Http/Controllers/Backend/Products/ProductTypeController.php
+++ app/Http/Controllers/Backend/Products/ProductTypeController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Backend\Products;
 
 use App\Http\Controllers\Controller;
+use App\Models\ProductType;
 use Illuminate\Http\Request;
+use Throwable;
 
 class ProductTypeController extends Controller
 {
@@ -15,6 +17,9 @@ class ProductTypeController extends Controller
     public function index()
     {
         //
+        $pageTitle = 'Loại sản phẩm';
+        $productTypes = ProductType::select('*')->with('products')->paginate(20);
+        return view('pages.product-type.list',compact('pageTitle','productTypes'));
     }
 
     /**
@@ -25,6 +30,9 @@ class ProductTypeController extends Controller
     public function create()
     {
         //
+        $pageTitle = 'Thêm loại sản phẩm';
+
+        return view('pages.product-type.add',compact('pageTitle'));
     }
 
     /**
@@ -35,7 +43,36 @@ class ProductTypeController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $rules = ['name'=>'required|unique:product_types|max:255|min:3',
+                'image'=>'image|mimes:jpg,png,jpeg|max:2040'];
+        $messages = [
+            'name.required'=> 'Bắt buộc nhập tên',
+            'name.min'=>'Tên ít nhất :min kí tự',
+            'name.max'=>'Tên tối đa :max kí tự',
+            'name.unique'=>'Tên này đã tồn tại',
+            'image.mimes'=>'Ảnh chỉ được là jpg, png, jpeg',
+            'image.max'=>'Ảnh kích thước tối đa là 2mb'
+        ];
+        $request->validate($rules,$messages);
+        try{
+            $type = new ProductType();
+            $type->fill($request->all());
+
+            // save upload 
+            if ($request->hasFile('image')) {
+                    $file = $request->file('image');
+                    $prefixImg = 'type-product';
+
+                    // upload file
+                    $type->image = fileUploader($file, $prefixImg, 'uploads/type-product');
+            }
+            $type->save();
+
+            return redirect()->back()->with('message','Thêm thành công loại sản phẩm!');
+        }catch(Throwable $e){
+            report($e->getMessage());
+            return redirect()->back()->with('exception','Có lỗi xảy ra, vui lòng thử lại sau!');
+        }
     }
 
     /**
@@ -57,7 +94,13 @@ class ProductTypeController extends Controller
      */
     public function edit($id)
     {
-        //
+        $pageTitle = 'Cập nhật loại sản phẩm';
+        if(!empty($id) && ProductType::find($id)){
+            $type = ProductType::find($id);
+            return view('pages.product-type.edit',compact('pageTitle','type'));
+        }
+        return redirect()->back()->with('erorr','Không tìm thấy loại sản phẩm');
+
     }
 
     /**
@@ -69,7 +112,40 @@ class ProductTypeController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $nameRule = 'unique:product_types';
+        if($request->method() == 'PUT'){
+            $nameRule = '';
+        }
+        $rules = ['name'=>'required|'.$nameRule.'|max:255|min:3',
+                'image'=>'image|mimes:jpg,png,jpeg|max:2040'];
+        $messages = [
+            'name.required'=> 'Bắt buộc nhập tên',
+            'name.min'=>'Tên ít nhất :min kí tự',
+            'name.max'=>'Tên tối đa :max kí tự',
+            'name.unique'=>'Tên này đã tồn tại',
+            'image.mimes'=>'Ảnh chỉ được là jpg, png, jpeg',
+            'image.max'=>'Ảnh kích thước tối đa là 2mb'
+        ];
+        $request->validate($rules,$messages);
+        try{
+            $type = ProductType::find($id);
+            $type->fill($request->all());
+
+            // save upload 
+            if ($request->hasFile('image')) {
+                    $file = $request->file('image');
+                    $prefixImg = 'type-product';
+
+                    // upload file
+                    $type->image = fileUploader($file, $prefixImg, 'uploads/type-product');
+            }
+            $type->save();
+
+            return redirect()->back()->with('message','Cập nhật thành công loại sản phẩm!');
+        }catch(Throwable $e){
+            report($e->getMessage());
+            return redirect()->back()->with('exception','Có lỗi xảy ra, vui lòng thử lại sau!');
+        }
     }
 
     /**
@@ -80,6 +156,12 @@ class ProductTypeController extends Controller
      */
     public function destroy($id)
     {
-        //
+        try{
+            ProductType::destroy($id);
+            return redirect()->back()->with('message','Xóa thành công!');
+        }catch(Throwable $e){
+            report($e->getMessage());
+            return redirect()->back()->with('erorr','Có lỗi xảy ra! Vui lòng thử lại!');
+        }
     }
 }
