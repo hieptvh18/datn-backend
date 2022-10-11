48c57b6b5a0f9c448e19919316195339e58d8e42
Hieunvph14434 <nguyenvanhieu2001gl@gmail.com>
2022-10-11 10:53:35 +0700

    PKNKD-100 sort, search,  filter listing order

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Kyslik\ColumnSortable\Sortable;
 
 class Order extends Model
 {
-    use HasFactory;
+    use HasFactory, Sortable;
     protected $table = 'orders';
     protected $fillable = [
         'customer_name',
@@ -17,8 +18,18 @@ class Order extends Model
         'code_bill',
         'payment_method',
         'description',
+        'date',
         'total'
     ];
+
+    public $sortable = [
+        'id',
+        'customer_name',
+        'customer_phone',
+        'date'
+    ];
+
+
     public function order_details (){
         return $this->belongsToMany(Order::class, 'order_details', 'order_id');
     }
