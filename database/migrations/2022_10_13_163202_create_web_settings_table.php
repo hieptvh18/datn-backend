36fbd60c381f40bd691dac6e741ece60088bc108
hieptvh <hieptvh18@gmail.com>
2022-10-13 23:50:24 +0700

    push db settings

--- database/migrations/2022_10_13_163202_create_web_settings_table.php
+++ database/migrations/2022_10_13_163202_create_web_settings_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('logo',255)->comment('logo: png,jpg,jpeg');
             $table->string('web_name',255)->comment('ten website|ten doanh nghiep');
             $table->string('base_url',255)->nullable()->comment('link website');
-            $table->string('phones',15)->nullable()->default('0989581167')->comment('so dien thoai lien he, luu nhieu so');
+            $table->string('phones',15)->default('0989581167')->comment('so dien thoai lien he, luu nhieu so');
             $table->string('email',255)->nullable()->default('nhakhoaducnghia@gmail.com')->comment('email');
             $table->string('facebook_url',255)->nullable()->default('https://facebook.com')->comment('fb');
             $table->string('twitter_url',255)->nullable()->default('https://facebook.com')->comment('twitter_url');
@@ -29,7 +29,7 @@ return new class extends Migration
             $table->string('close_time')->comment('gio dong cua');
             $table->date('start_date')->comment('ngay bat dau lam viec trong tuan');
             $table->date('end_date')->comment('ngay ket thuc trong tuan');
-            $table->text('short_introduce')->comment('gioi thieu ngan')->default('Triết lý của ĐỨC NGHĨA sẽ giúp bạn khỏe mạnh, hạnh phúc vì chúng tôi hiểu vai trò quan trọng trong sức khỏe răng miệng của bạn.');
+            $table->text('short_introduce')->nullable()->comment('gioi thieu ngan')->default('Triết lý của ĐỨC NGHĨA sẽ giúp bạn khỏe mạnh, hạnh phúc vì chúng tôi hiểu vai trò quan trọng trong sức khỏe răng miệng của bạn.');
             $table->longText('introduce')->comment('gioi thieu chi tiet ve chung toi');
             $table->timestamps();
         });
