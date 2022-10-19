0606d51441b8782afa89508cd1a00072bbd6a048
dukeondaB <79830923+dukeondaB@users.noreply.github.com>
2022-10-19 23:06:34 +0700

    PKNKD-149 api equipment

---
+++ app/Http/Controllers/Api/EquipmentController.php
@@ -0,0 +1,27 @@
+<?php
+namespace App\Http\Controllers\Api;
+use App\Http\Controllers\Controller;
+use App\Models\Equipment;
+
+class EquipmentController extends Controller
+{
+    public function list()
+    {
+        try{
+            $listEquip = Equipment::query()->get(['id','name','price','image','size','short_desc']);
+            return response()->json([
+                'success' => true,
+                'message'=> 'Danh sách trang thiết bị',
+                'data' => $listEquip->toArray()
+            ]);
+        }
+     catch (\Throwable $th) {
+        report($th->getMessage());
+        return response()->json([
+            'success' => false,
+            'message'=> 'Đã xảy ra lỗi!'.$th->getMessage(),
+            'data' => []
+        ]);
+    }
+    }
+}
